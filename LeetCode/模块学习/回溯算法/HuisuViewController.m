f078782f9dd5440bfece104cd0849b5f7fb0a9fc
home-zhangjianjun <>
2021-01-12 01:31:00 +0800

    回溯

--- LeetCode/模块学习/回溯算法/HuisuViewController.m
+++ LeetCode/模块学习/回溯算法/HuisuViewController.m
@@ -15,6 +15,10 @@
 @implementation HuisuViewController
 
 /*
+ 回文串: 是一个正读和反读都一样的字符串，比如“level”或者“noon”等等就是回文串
+ */
+
+/*
  ** 回溯算法 **
  - 如果解决一个问题有多个步骤，每一个步骤有多种方法，题目又要我们找出所有的方法，可以使用回溯算法；
  - 回溯算法是在一棵树上的 深度优先遍历（因为要找所有的解，所以需要遍历）；
@@ -54,7 +58,11 @@
                             @"组合总和III",
                             @"电话号码的字母组合",
                             @"组合总和",
-                            @"组合总和II"
+                            @"组合总和II",
+                            @"分割回文串",
+                            @"复原IP地址",
+                            @"子集",
+                            @"子集II"
     ]];
 }
 
@@ -75,6 +83,18 @@
             break;
         case 4:
             [self action4];
+            break;
+        case 5:
+            [self action5];
+            break;
+        case 6:
+            [self action6];
+            break;
+        case 7:
+            [self action7];
+            break;
+        case 8:
+            [self action8];
             break;
            
         default:
@@ -337,6 +357,215 @@ static NSInteger sum = 0;
            startIndex:(NSInteger)startIndex
 {
 
+}
+
+- (void)action5
+{
+    // 131 分割回文串
+    // 给定一个字符串 s，将 s 分割成一些子串，使每个子串都是回文串。
+    /*
+     示例:
+     输入: "aab"
+     输出:
+     [
+     ["aa","b"],
+     ["a","a","b"]
+     ]
+     */
+    NSString *s = @"aab";
+    NSMutableArray *result = [NSMutableArray array];
+    NSMutableArray *path = [NSMutableArray array];
+    [self action5Result:result path:path s:s startIndex:0];
+    NSLog(@"--%@", result);
+    
+}
+
+- (void)action5Result:(NSMutableArray *)result
+                 path:(NSMutableArray <NSString *>*)path
+                    s:(NSString *)s
+           startIndex:(NSInteger)startIndex
+{
+    if (startIndex >= s.length) {
+        [result addObject:path.copy];
+        return;
+    }
+    
+    for (NSInteger i = startIndex; i < s.length ; i++) {
+        
+        if ([self isPalindrome:s start:startIndex end:i]) {
+            NSString *subs = [s substringWithRange:NSMakeRange(startIndex, i - startIndex + 1)];
+            [path addObject:subs];
+        } else {
+            continue;
+        }
+        
+        [self action5Result:result path:path s:s startIndex:i + 1];
+        [path removeLastObject];
+    }
+}
+
+- (BOOL)isPalindrome:(NSString *)str start:(NSInteger)start end:(NSInteger)end
+{
+    for (NSInteger i = start, j = end; i < j; i++, j--) {
+        NSString *left = [str substringWithRange:NSMakeRange(i, 1)];
+        NSString *right = [str substringWithRange:NSMakeRange(j, 1)];
+        if (left != right) {
+            return NO;
+        }
+    }
+    return YES;
+}
+
+- (void)action6
+{
+    // 93 复原IP地址
+    /*
+     给定一个只包含数字的字符串，复原它并返回所有可能的 IP 地址格式。
+     有效的 IP 地址 正好由四个整数（每个整数位于 0 到 255 之间组成，且不能含有前导 0），整数之间用 '.' 分隔。
+     例如："0.1.2.201" 和 "192.168.1.1" 是 有效的 IP 地址，但是 "0.011.255.245"、"192.168.1.312" 和 "192.168@1.1" 是 无效的 IP 地址。
+     
+     示例 1：
+     输入：s = "25525511135"
+     输出：["255.255.11.135","255.255.111.35"]
+
+     示例 2：
+     输入：s = "0000"
+     输出：["0.0.0.0"]
+
+     示例 3：输入：s = "1111"
+     输出：["1.1.1.1"]
+
+     示例 4：输入：s = "010010"
+     输出：["0.10.0.10","0.100.1.0"]
+
+     示例 5：输入：s = "101023"
+     输出：["1.0.10.23","1.0.102.3","10.1.0.23","10.10.2.3","101.0.2.3"]
+
+     提示：
+     0 <= s.length <= 3000
+     s 仅由数字组成
+     */
+    
+//    NSString *s = @"25525511135";
+//    NSString *s = @"0000";
+//    NSString *s = @"1111";
+//    NSString *s = @"010010";
+    NSString *s = @"101023";
+    NSMutableArray *result = [NSMutableArray array];
+    NSMutableArray *path = [NSMutableArray array];
+    
+    [self action6S:s res:result path:path startIndex:0];
+    NSLog(@"%@", result);
+}
+
+- (void)action6S:(NSString *)s res:(NSMutableArray <NSString *>*)res path:(NSMutableArray <NSString *>*)path startIndex:(NSInteger)startIndex
+{
+    // 当已经截取三个的时候 第四个必须是合法的才算ip地址 且截取点要小于s长度
+    if (path.count == 3 && startIndex < s.length) {
+        NSString *last = [s substringWithRange:NSMakeRange(startIndex, s.length - startIndex)];
+        
+        if ([self legalCommit:last]) {
+            [path addObject:last];
+            [res addObject:path.copy];
+            [path removeLastObject];
+        }
+        return;
+    }
+    
+    for (NSInteger i = startIndex; i < s.length; i++) {
+        NSString *sub = [s substringWithRange:NSMakeRange(startIndex, i - startIndex + 1)];
+        NSInteger length = sub.length;
+        if ((length == 3 && sub.integerValue > 255) || length > 4) {
+            return;
+        }
+        
+        if ([self legalCommit:sub]) {
+            [path addObject:sub];
+        } else {
+            break;
+        }
+        
+        [self action6S:s res:res path:path startIndex:i + 1];
+        [path removeLastObject];
+    }
+}
+
+- (BOOL)legalCommit:(NSString *)commit
+{
+    if (commit.length > 1 && [commit hasPrefix:@"0"]) {
+        return NO;
+    }
+    NSInteger commitInt = commit.integerValue;
+    if (commitInt >= 0 && commitInt <= 255) {
+        return YES;
+    }
+    return NO;
+}
+
+- (void)action7
+{
+    // 78. 子集
+    /*
+     给定一组不含重复元素的整数数组 nums，返回该数组所有可能的子集（幂集）。
+     说明：解集不能包含重复的子集。
+     
+     示例: 输入: nums = [1,2,3]
+     输出:
+     [
+       [3],
+       [1],
+       [2],
+       [1,2,3],
+       [1,3],
+       [2,3],
+       [1,2],
+       []
+     ]
+     */
+    NSMutableArray *result = [NSMutableArray array];
+    NSMutableArray *path = [NSMutableArray array];
+    NSArray *nums = @[@(1),@(2),@(3)];
+    [self action7nums:nums res:result path:path startIndex:0];
+    NSLog(@"%@", result);
+}
+
+- (void)action7nums:(NSArray <NSNumber *>*)nums
+                res:(NSMutableArray <NSNumber *>*)res
+               path:(NSMutableArray <NSNumber *>*)path
+         startIndex:(NSInteger)startIndex
+{
+    [res addObject:path.copy];
+    if (startIndex == nums.count) {
+        return;
+    }
+    
+    for (NSInteger i = startIndex; i < nums.count; i++) {
+        [path addObject:nums[i]];
+        NSLog(@"%@", path);
+        [self action7nums:nums res:res path:path startIndex:i+1];
+        [path removeLastObject];
+    }
+}
+
+- (void)action8
+{
+    // 90 子集II
+    /*
+     给定一个可能包含重复元素的整数数组 nums，返回该数组所有可能的子集（幂集）。
+     说明：解集不能包含重复的子集。
+     
+     示例:
+     输入: [1,2,2]
+     输出:
+     [
+     [2],
+     [1],
+     [1,2,2],
+     [2,2],
+     [1,2],
+     []
+     ]
+     */
 }
 
 @end
