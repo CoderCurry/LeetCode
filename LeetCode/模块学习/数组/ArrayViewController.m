47aefa486bb45c89a03451152d204281cf6a99a3
home-zhangjianjun <>
2021-01-19 02:35:50 +0800

    复盘

--- LeetCode/模块学习/数组/ArrayViewController.m
+++ LeetCode/模块学习/数组/ArrayViewController.m
@@ -65,19 +65,19 @@
 {
     // 编号35：搜索插入位置
     // [1,3,5,6], 5
+    /*
+     给定一个排序数组和一个目标值，在数组中找到目标值，并返回其索引。如果目标值不存在于数组中，返回它将会被按顺序插入的位置。
+     你可以假设数组中无重复元素。
+     */
     NSMutableArray *arr = @[@(1),@(3),@(5),@(6)].mutableCopy;
-    NSInteger target = 5;
-    NSInteger index = [self searchIndex:arr target:target];
-    NSInteger target1 = 2;
-    NSInteger index1 = [self searchIndex:arr target:target1];
-    NSInteger target2 = 7;
-    NSInteger index2 = [self searchIndex:arr target:target2];
-    NSInteger target3 = 0;
-    NSInteger index3 = [self searchIndex:arr target:target3];
+    NSArray *targets = @[@(5),@(2),@(7),@(0)];
     NSLog(@"答案 2 1 4 0");
-    NSLog(@"结果 %ld %ld %ld %ld", index, index1, index2, index3);
+    for (NSInteger i = 0; i < targets.count; i++) {
+        NSInteger target = [targets[i] integerValue];
+        NSInteger index = [self action0SearchIndex:arr target:target];
+        NSLog(@"结果 %ld", index);
+    }
 }
-
 // 利用二分法 查找插入index
 /*
  创建left right 和mid
@@ -87,7 +87,7 @@
  
  视频: https://leetcode-cn.com/problems/search-insert-position/solution/sou-suo-cha-ru-wei-zhi-by-leetcode-solution/
  */
-- (NSInteger)searchIndex:(NSMutableArray <NSNumber *>*)sortArray target:(NSInteger)target
+- (NSInteger)action0SearchIndex:(NSMutableArray <NSNumber *>*)sortArray target:(NSInteger)target
 {
     NSInteger left = 0;
     NSInteger right = sortArray.count - 1;
@@ -112,25 +112,24 @@
     // nums = [3,2,2,3], val = 3 -> 2 [2,2]
     // nums = [0,1,2,2,3,0,4,2], val = 2, -> 5 [0,1,3,0,4]
     
-    NSMutableArray *nums = @[@(3),@(2),@(2),@(3)].mutableCopy;
-    NSInteger val = 3;
+    
+    NSMutableArray *nums0 = @[@(3),@(2),@(2),@(3)].mutableCopy;
     NSMutableArray *nums1 = @[@(0),@(1),@(2),@(2),@(3),@(0),@(4),@(2)].mutableCopy;
-    NSInteger val1 = 2;
-    
-    NSInteger index = [self nums:nums val:val];
-    NSInteger index1 = [self nums:nums1 val:val1];
-    NSLog(@"- %ld %ld", index, index1);
-    
+    NSArray *nums = @[nums0, nums1];
+    NSArray *vals = @[@(3), @(2)];
+    NSLog(@"答案 2 5");
+    for (NSInteger i = 0; i < nums.count; i++) {
+        NSInteger index = [self action1Nums:nums[i] val:[vals[i] integerValue]];
+        NSLog(@"%ld", index);
+    }
 }
 
 // 双指针 这里nums已经被修改过 用nums 取前slow个 就是新数组
-- (NSInteger)nums:(NSMutableArray <NSNumber *>*)nums val:(NSInteger)val
+- (NSInteger)action1Nums:(NSMutableArray <NSNumber *>*)nums val:(NSInteger)val
 {
     NSInteger fast = 0;
     NSInteger slow = 0;
-    
-    for (int i = 0; i < nums.count; i++) {
-        
+    for (NSInteger i = 1; i < nums.count; i++) {
         if (nums[fast].integerValue == val) {
             fast++;
         } else {
@@ -150,11 +149,11 @@
     // sort [-4, -1, -1, 0, 1, 2]
     
     NSArray *nums = @[@(-1), @(0), @(1), @(2), @(-1), @(-4)];
-    NSArray *result = [self nums:nums];
-    NSLog(@"arr:%@", result);
+    NSArray *result = [self action3Nums:nums];
+    NSLog(@"retult :%@", result);
 }
 
-- (NSArray *)nums:(NSArray <NSNumber *>*)nums
+- (NSArray *)action3Nums:(NSArray <NSNumber *>*)nums
 {
     nums = [nums sortedArrayUsingSelector:@selector(compare:)];
     NSMutableArray *result = [NSMutableArray arrayWithCapacity:0];
@@ -205,59 +204,58 @@
     //
     // [1, 0, -1, 0, -2, 2]  target = 0
     NSArray <NSNumber *>*nums = @[@(1), @(0), @(-1), @(0), @(-2), @(2)];
-    nums = [nums sortedArrayUsingSelector:@selector(compare:)];
-    
-    NSArray *result = [self nums:nums target:0];
-    NSLog(@"arr:%@", result);
+    NSArray *result = [self action3Nums:nums target:0];
+    NSLog(@"result :%@", result);
 
 }
 
 // 增加了减枝操作 核心其实就是for 包裹 3数和
-- (NSArray *)nums:(NSArray <NSNumber *>*)nums target:(NSInteger)target
+- (NSArray *)action3Nums:(NSArray <NSNumber *>*)nums target:(NSInteger)target
 {
+    NSArray <NSNumber *>*sortNums = [nums sortedArrayUsingSelector:@selector(compare:)];
     NSMutableArray *result = [NSMutableArray arrayWithCapacity:0];
 
-    for (NSInteger i = 0; i < nums.count - 3; i++) {
+    for (NSInteger i = 0; i < sortNums.count - 3; i++) {
     
         // 保证第一个数是从小到大排序 将第一个数去重
-        if (i > 0 && nums[i] == nums[i - 1]) {
+        if (i > 0 && sortNums[i] == sortNums[i - 1]) {
             continue;
         }
 
         // i下 最小值 > target 就返回
-        if (nums[i].integerValue + nums[i + 1].integerValue + nums[i + 2].integerValue + nums[i + 3].integerValue > target) {
+        if (sortNums[i].integerValue + sortNums[i + 1].integerValue + sortNums[i + 2].integerValue + sortNums[i + 3].integerValue > target) {
             break;
         }
         
         // i下 最大值 < target 就提升i
-        if (nums[i].integerValue + nums[nums.count - 3].integerValue + nums[nums.count - 2].integerValue + nums[nums.count - 1].integerValue < target) {
+        if (sortNums[i].integerValue + sortNums[sortNums.count - 3].integerValue + sortNums[sortNums.count - 2].integerValue + sortNums[sortNums.count - 1].integerValue < target) {
             continue;
         }
 
 
-        for (NSInteger j = i + 1; j < nums.count - 2; j++) {
+        for (NSInteger j = i + 1; j < sortNums.count - 2; j++) {
             // 去重
-            if (j > i + 1 && nums[j] == nums[j - 1]) {
+            if (j > i + 1 && sortNums[j] == sortNums[j - 1]) {
                 continue;
             }
             
             // i j下 最小值 > target 就返回
-            if (nums[i].integerValue + nums[j].integerValue + nums[j + 1].integerValue + nums[j + 2].integerValue > target) {
+            if (sortNums[i].integerValue + sortNums[j].integerValue + sortNums[j + 1].integerValue + sortNums[j + 2].integerValue > target) {
                 break;
             }
             
             // i j下 最大值 < target 就提升j
-            if (nums[i].integerValue + nums[j].integerValue + nums[nums.count - 2].integerValue + nums[nums.count - 1].integerValue < target) {
+            if (sortNums[i].integerValue + sortNums[j].integerValue + sortNums[sortNums.count - 2].integerValue + sortNums[sortNums.count - 1].integerValue < target) {
                 continue;
             }
 
             NSInteger left = j + 1;
-            NSInteger right = nums.count - 1;
+            NSInteger right = sortNums.count - 1;
             while (left < right) {
-                NSInteger iValue = nums[i].integerValue;
-                NSInteger jValue = nums[j].integerValue;
-                NSInteger leftValue = nums[left].integerValue;
-                NSInteger rightValue = nums[right].integerValue;
+                NSInteger iValue = sortNums[i].integerValue;
+                NSInteger jValue = sortNums[j].integerValue;
+                NSInteger leftValue = sortNums[left].integerValue;
+                NSInteger rightValue = sortNums[right].integerValue;
                 if (iValue + jValue + leftValue + rightValue > 0) {
                     right--;
                 } else if (iValue + jValue + leftValue + rightValue < 0) {
@@ -265,8 +263,8 @@
                 } else {
                     [result addObject:@[@(iValue), @(jValue), @(leftValue), @(rightValue)]];
                     // 将第二三个数去重 eg.@[@(-1), @(0), @(1), @(2), @(-1), @(-4), @(1), @(0)];
-                    while (right > left && nums[right] == nums[right - 1]) right--;
-                    while (right > left && nums[left] == nums[left + 1]) left++;
+                    while (right > left && sortNums[right] == sortNums[right - 1]) right--;
+                    while (right > left && sortNums[left] == sortNums[left + 1]) left++;
                     // 找到0后 2边必须收缩才可能再次产生0
                     right--;
                     left++;
